580d3700068c7328c2fae31e0393f4566368be36
bezalel <bezalel.dev@gmail.com>
2017-08-24 18:00:05 +0900

    truffle.js setup (ropsten, rinkeby, live), testrpc setup

--- truffle.js
+++ truffle.js
@@ -4,6 +4,27 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    ropsten: {
+      network_id: 3, // ropsten testnet
+      host: "localhost",
+      port: 8546,
+      gas: 4712384,
+      gasPrice: 900000000000 // 900 Shannon
+    },
+    rinkeby: {
+      network_id: 4, // rinkeby testnet
+      host: "localhost",
+      port: 8547,
+      gas: 4500000
+      // gasPrice: 100000000000 // defulat 100 Shannon
+    },
+    live: {
+      network_id: 1,
+      host: "localhost",
+      port: 8548,
+      gas: 4712384
+      // gasPrice: 100000000000 // default 100 Shannon
     }
   }
 };
